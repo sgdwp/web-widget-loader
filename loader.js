0323acf20623f0658a04fed1061b05f2f12a17bb
sgdwp <72244970+sgdwp@users.noreply.github.com>
2025-06-24 10:01:55 +0800

    Update loader.js
    update loader

--- loader.js
+++ loader.js
@@ -1,31 +1,34 @@
 (() => {
   const script = document.currentScript;
-  console.log("Loader file loaded")
-    const hostname = window.location.hostname;
+
+  const hostname = window.location.hostname;
   if (hostname) {
     console.log("hostname", hostname);
   }
-  
+
   // const URL = "https://ai-chatbot-2honfku3e-chin-weis-projects.vercel.app";
   // const URL = "https://ai-chatbot-ajpxv3mti-chin-weis-projects.vercel.app";
-    const URL = "http://localhost:3000/query";
+
+  // const URL = "https://sstabbgovtechomauatqatg.z23.web.core.windows.net/";
+  const URL = "http://localhost:3000/query";
   const loadWidget = () => {
     const widget = document.createElement("div");
 
-
     const widgetStyle = widget.style;
-    widgetStyle.display = "none";
+    widgetStyle.display = "block";
     widgetStyle.boxSizing = "border-box";
-    widgetStyle.width = "800px";
-    widgetStyle.height = "1000px";
+    widgetStyle.width = "520px";
+    widgetStyle.height = "580px";
     widgetStyle.position = "absolute";
-    widgetStyle.bottom = "40px";
+    widgetStyle.bottom = "55px";
     widgetStyle.right = "40px";
-
+    widgetStyle.backgroundColor = "#fff";
+    widgetStyle.overflow = "hidden";
+    widgetStyle.visibility = "hidden";
     const iframe = document.createElement("iframe");
 
     const iframeStyle = iframe.style;
-    iframeStyle.boxSizing = "borderBox";
+    iframeStyle.boxSizing = "border-box";
     iframeStyle.position = "absolute";
     iframeStyle.right = 0;
     iframeStyle.top = 0;
@@ -35,15 +38,10 @@
     iframeStyle.border = 0;
     iframeStyle.margin = 0;
     iframeStyle.padding = 0;
-    iframeStyle.width = "800px"; //width
-
     widget.appendChild(iframe);
 
     const greeting = script.getAttribute("data-greeting");
-    if (greeting) {
-      console.log("greeting", greeting);
-    }
-    
+
     const api = {
       sendMessage: (message) => {
         iframe.contentWindow.postMessage(
@@ -55,21 +53,26 @@
       },
 
       show: () => {
-        widget.style.display = "block";
+        // widget.style.display = "block";
+        widgetStyle.visibility = "visible";
       },
 
       hide: () => {
-        widget.style.display = "none";
+        // widget.style.display = "none";
+        widgetStyle.visibility = "hidden";
       },
 
       toggle: () => {
-        const display = window.getComputedStyle(widget, null).display;
-        widget.style.display = display === "none" ? "block" : "none";
+        // const display = window.getComputedStyle(widget, null).display;
+        // widget.style.display = display === "none" ? "block" : "none";
+
+        const visibility = window.getComputedStyle(widget).visibility;
+        widget.style.visibility =
+          visibility === "hidden" ? "visible" : "hidden";
       },
 
       onHide: () => {},
     };
-
     iframe.addEventListener("load", () => {
       window.addEventListener("getWidgetApi", () => {
         const event = new CustomEvent("widgetApi", { detail: api });
@@ -88,33 +91,34 @@
       });
 
       iframe.contentWindow.postMessage({ greeting }, URL);
-      widgetStyle.display = "block";
+      // widgetStyle.display = "none";
+      widgetStyle.visibility = "hidden";
     });
 
     const license = script.getAttribute("data-license");
-    if (license) {
-      console.log("license", license);
-    }
-    
     // const widgetUrl = `http://localhost:3000?license=${license}`;
-    const widgetUrl = `${URL}?license=${license}`;
+    const widgetUrl = `${URL}?license=${license}&hostname=${hostname}`;
     iframe.src = widgetUrl;
 
     document.body.appendChild(widget);
 
     // Create the toggle button
     const toggleButton = document.createElement("button");
-    toggleButton.textContent = "Toggle Widget";
+    toggleButton.textContent = "Ask a question";
     const buttonStyle = toggleButton.style;
+
     buttonStyle.position = "absolute";
-    buttonStyle.bottom = "10px";
+    buttonStyle.bottom = "8px";
     buttonStyle.right = "40px";
     buttonStyle.padding = "10px 20px";
-    buttonStyle.backgroundColor = "#007bff";
+    buttonStyle.backgroundImage =
+      "linear-gradient(to right, #491DB6, #5925DC, #7E55E4)";
     buttonStyle.color = "#fff";
     buttonStyle.border = "none";
     buttonStyle.borderRadius = "5px";
     buttonStyle.cursor = "pointer";
+    buttonStyle.fontWeight = "bold";
+    buttonStyle.width = "180px";
 
     // Attach click event to toggle the widget
     toggleButton.addEventListener("click", () => {
